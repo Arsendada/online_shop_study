06a0cbea73bd58dad36f01f845e4b2012fe17246
Arsen <arsendada@mail.ru>
2022-12-15 23:22:48 +0300

    add celery and order_created task

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 from cart.cart import Cart
 
 
@@ -12,12 +13,16 @@ def order_create(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
-                          'orders/order/created.html')
+                          'orders/order/created.html',
+                          {'order': order})
     else:
         form = OrderCreateForm()
     return render(request,
