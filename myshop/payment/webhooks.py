a13933b193084cb8b8b3c168c98b772587f9b754
Arsen <arsendada@mail.ru>
2022-12-17 01:47:23 +0300

    created webhooks stripe function

--- myshop/payment/webhooks.py
+++ myshop/payment/webhooks.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
-from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -36,7 +35,5 @@ def stripe_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
-            # launch asynchronous task
-            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
